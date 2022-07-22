d36f85bfee239c8a82e95c748b41daa22e0acfd0
Adam <adam15free@hotmail.com>
2022-07-22 16:35:16 -0400

    syntax update

--- pwa-app/pages/index.js
+++ pwa-app/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { useState } from 'react'
-import "./styles/App.css"
+
 
 export default function Home() {
     const [counter, setCounter] = useState(0);
